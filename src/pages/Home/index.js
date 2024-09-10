da3eb8c7dc1442c0210c012915d27ea2ce8d0a22
debcamila <camiladeborah72@gmail.com>
2024-09-10 15:05:37 -0300

    feat: added home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,49 @@
+import { useState } from "react";
+import "./home.css";
+
+import { Link } from "react-router-dom";
+
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleLogin(e) {
+    e.preventDefault();
+
+    if (email !== "" && password !== "") {
+      alert("registrar");
+    } else {
+      alert("Preencha todos os campos.");
+    }
+  }
+
   return (
-    <div>
-      <h1>Página Home</h1>
+    <div className="home_container">
+      <h1>Lista de Tarefas</h1>
+      <span>Gerencie suas tarefas de forma fácil.</span>
+
+      <form className="home_form" onSubmit={handleLogin}>
+        <input
+          type="text"
+          placeholder="Digite seu e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <input
+          autoComplete={false}
+          type="password"
+          placeholder="Digite sua senha"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <button type="submit">Acessar</button>
+      </form>
+
+      <Link className="home_link" to="/register">
+        Não possui uma conta? Cadastra-se
+      </Link>
     </div>
   );
 }
